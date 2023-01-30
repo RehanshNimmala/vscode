5fdffbfa1f93aed1c7745c0614aa64c48dea6c61
NAVEEN\Naveen kumar <naveennimmala1818@gmail.com>
2023-01-29 22:06:42 -0600

    hello world added

---
+++ 01_helloworld.py
@@ -0,0 +1,53 @@
+print('hello World')
+total=1+\
+3+\
+11
+print(total)
+
+
+word='Trinity'
+sentence="Matrix Resurrenction"
+
+paragraph="Lawrence "
+#comments
+#Hello World
+
+print("my name")# comments
+
+#multiline comment
+'''
+printing
+status of my hello world
+'''
+
+import sys;
+x='parameter';sys.stdout.write(x+'\n')
+
+number=20
+if number<5:
+    print("less than 5")
+elif number>10:
+    print("less than 10")
+        
+age= 16 #integer 
+name="Naveen"
+print (age)
+a=b=c=1
+       
+d,e,f=1,2,'navin'
+       
+       
+myNum=20
+del myNum
+del a,b,c
+
+name='navin'
+#name2=name*name
+
+str='navin'
+print (str)
+print (str[0])# first letter statrs with 0, navin has 04
+print(str[2:4])
+print(str[2:])# starts with 2 and continues
+
+# does not have char
